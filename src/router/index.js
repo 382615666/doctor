ba0e20e81dd7ea2e874ede2d28f879be364bcbd6
王湘赣 <gana@wangxianggandeMacBook-Air.local>
2019-03-04 15:42:45 +0800

    a

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,8 @@ const router = new VueRouter({
   routes
 });
 
-const time = new Date('2019-03-01 12:00:00').getTime();
-const nowTime = Date.now();
-
 router.beforeEach((to, from, next) => {
-  if (nowTime > time) {
-    next('/error');
-  } else if (to.fullPath === '/error') {
+  if (to.fullPath === '/error') {
     to.meta.title = from.meta.title || to.meta.title;
   } else {
     next();
